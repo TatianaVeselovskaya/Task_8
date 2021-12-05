69beedf69596406005664a1bf6cb739da06ee8cb
Татьяна <veselovskayt@mail.ru>
2021-12-05 12:19:34 +0700

    Добавьте файлы проекта.

---
+++ Task_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"d:\BIM");
+
+            Console.WriteLine("============Список папок=============");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("*****Список подкаталогов и файлов*****");
+                foreach (var it in item.GetDirectories())
+                { Console.WriteLine(it.Name); }
+                foreach (var it in item.GetFiles())
+                { Console.WriteLine(it.Name); }
+                Console.WriteLine();
+            }
+            Console.WriteLine("=============Список файлов=============");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_8_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"test.txt";
+            double sum = 0;
+            FileStream fileStream = null;
+
+            if (!File.Exists(filePath))
+                fileStream = File.Create(filePath);
+            else
+                fileStream = File.Open(filePath, FileMode.Create);
+
+            StreamWriter output = new StreamWriter(fileStream);
+
+            int[] array = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = random.Next(0,10);
+                output.WriteLine(random.Next());
+            }
+            output.Close();
+
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string s in lines)
+            {
+                sum = sum + Convert.ToDouble(s);
+            }
+
+            Console.WriteLine("Сумма = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
